a1100fee86cb5b91b2b227baf4e2fa2f37ada7b9
Victor-OT <crowbreaker96@hotmail.com>
2024-05-02 18:41:50 -0600

    Se realizaron ejercicios sobre números y operaciones matemáticas

---
+++ number.js
@@ -0,0 +1,39 @@
+// 1. Tipo entero y decimal
+
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal)
+
+// 2. Notación cientifica
+const cientifico = 5e3
+
+// 3. Infinitos y NaN
+const infinito = Infinity
+constnoEsUnNumero = NaN
+
+//Operaciones Aritméticas
+//1. Suma, Resta. Multiplicación y División
+
+const suma = 3+4
+const Resta = 4-4
+const multiplicacion = 4*7
+const division = 16/2
+
+//2. Módulo y Exponenciación
+
+const modulo = 15%8
+const exponenciacion = 2 ** 3
+
+//Precisión
+const resultado = 0.1 + 0.2
+console.log(resultado)
+console.log(resultado.toFixed(1))
+console.log(resultado === 0.3)
+
+//Operaciones avanzadas
+const raizCuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-7)
+const aleatorio = Math.random()
+console.log(raizCuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
