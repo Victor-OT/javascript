824b4ec1191a3cc2dc3cb4b633219c4ff2fcd0cc
Victor-OT <crowbreaker96@hotmail.com>
2024-05-02 18:53:37 -0600

    Se creó archivo de booleans y se realizaron ejercicios de conversión implícita y explícita

---
+++ booleans.js
@@ -0,0 +1,15 @@
+const isActive = true
+const hasPermission = false
+
+//Conversión Implícita
+const resultado = 5 > 3
+console.log(resultado)
+
+const name = 'Alien'
+console.log(!!name)
+
+//Conversión Explícita
+const value = 0
+const otherValue = -24
+const explicitBoolean = Boolean(otherValue)
+console.log(explicitBoolean)
