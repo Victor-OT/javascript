b4879b474b0ab957a4f9653b0dd48db81db2ff7a
Victor-OT <crowbreaker96@hotmail.com>
2024-05-05 18:16:47 -0600

    Se creó archivo de funciones puras e impuras

---
+++ puras-impuras.js
@@ -0,0 +1,31 @@
+//funciones puras
+
+//Side Effects
+//1.- Modificar variables globales
+//2. Modificar variablesglobales
+//3.- Modificar parámetros
+//4.- Solicitudes HTTP
+//5.- Imprimir Mensajes en Pantalla O consola
+//6.- Manipulación del DOM
+//7.- Obtener la hora actual
+
+function sum (a, b)
+{
+    return a + b
+}
+
+//Funciones Impuras
+
+function suma (a, b)
+{
+    console.log('a: ' a)
+    return a + b
+}
+
+let total = 0
+
+function sumWithSideEffect(a)
+{
+    total += a
+    return total
+}
