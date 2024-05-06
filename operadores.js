a82a2424534a7b657ca8f144cf9cbb08d93118c6
Victor-OT <crowbreaker96@hotmail.com>
2024-05-06 17:19:09 -0600

    Se creó archivo de tipos de operadores

---
+++ operadores.js
@@ -0,0 +1,24 @@
+/* 1. Operadores de comparación
+//Compara valores
+==
+
+Compara valores y tipos de dato
+===
+
+Diferente de (solo valores)
+!=
+Diferente de (valores y tipos de dato)
+!==
+
+Mayor qué - Menor qué
+ < <= > >= 
+ 
+ Operadores de lógica
+ && || !()b
+ */
+
+ const a = 10
+ const b = 20
+ const c = '10'
+
+console.log( a == b)
