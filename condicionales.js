c45ec810d7dfa7ce8814c17d36371b10f879e22f
Victor-OT <crowbreaker96@hotmail.com>
2024-05-06 19:00:52 -0600

    Se crea archivo de condicionales, se inicia el ejercicio pero sigue incompleto

---
+++ condicionales.js
@@ -0,0 +1,16 @@
+//Condición if
+let nombre = 'Crow'
+
+if(nombre === 'Victor')
+{
+    console.log('Hola Victor')
+}
+else
+{
+    console.log('No te conozco')
+}
+
+//Ejercicio
+const numeroSecreto = Math.floor(Math.random() * 10 + 1);
+const numeroJugador = parseInt(prompt("Adivina el número secreto entre el 1 y el 10"))
+console.log(numeroJugador)
