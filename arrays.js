ee02d549365d63fd704c0f42c8911a14344bb032
Victor-OT <crowbreaker96@hotmail.com>
2024-05-10 13:00:37 -0600

    Se c reó archivo de tipos de arrays

---
+++ arrays.js
@@ -0,0 +1,28 @@
+const array = Array (1,2,3,4,5)
+console.log(array)
+
+const array2 = new Array (1,2,3,4,5)
+console.log(array2)
+
+const arrayMasChido = [1]
+console.log(arrayMasChido)
+
+const emptyArray = []
+console.log(emptyArray)
+
+const arrayMixto = 
+[
+    1,
+    2.5,
+    'Hola Perrines',
+    {
+        nombre: 'Crow',
+        edad: 15
+    },
+    true
+]
+console.log(arrayMixto)
+console.log(arrayMixto[0])
+console.log(arrayMixto[3])
+
+console.log(arrayMixto.length)
