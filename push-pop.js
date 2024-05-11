9c1959c587cfa927fafa2f6d0a269082ebfaff47
Victor-OT <crowbreaker96@hotmail.com>
2024-05-11 09:31:19 -0600

    Se crea archivo de metodo push y pop

---
+++ push-pop.js
@@ -0,0 +1,10 @@
+const countries = ['Mexico','Colombia','Argentina']
+console.log(countries)
+
+const newCountries = countries.push('Chile', 'Venezuela')
+console.log(countries)
+console.log(newCountries)
+
+const removedCountries = countries.pop()
+console.log(countries)
+console.log(removedCountries)
