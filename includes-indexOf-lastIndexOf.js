4c27257835fbee3a11f42d8f5615bd96f398316a
Victor-OT <85083940+Victor-OT@users.noreply.github.com>
2024-05-15 19:50:08 +0000

    Se creó archivo de metodos includes, index of y last index of

---
+++ includes-indexOf-lastIndexOf.js
@@ -0,0 +1,24 @@
+//Includes numbers
+const numbers = [1, 2, 3, 4, 5]
+
+const result1 = numbers.includes(3)
+console.log(result1)
+
+const result2 = numbers.includes(8)
+console.log(result2)
+
+//Index of
+const fruits = ['apple', 'cherry', 'grapes', 'mango']
+const index1 = fruits.indexOf('grapes')
+console.log(index1)
+
+const index2 = fruits.indexOf('blueberry')
+console.log(index1)
+
+//LastIndexOf
+const otherNumbers = [2,4,6,8,10,6]
+const lastIndex1 = otherNumbers.lastIndexOf(6)
+console.log(lastIndex1)
+
+const lastIndex2 = otherNumbers.lastIndexOf(3)
+console.log(lastIndex2)
